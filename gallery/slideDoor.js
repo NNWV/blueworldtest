57bf71c737e5a6a40ddc3515ace60fda0d5a60df
NathanGassaway <nathangway@gmail.com>
2022-09-19 10:22:06 -0400

    editing mic button, removing surplus image files

--- gallery/slideDoor.js
+++ gallery/slideDoor.js
@@ -1,3 +1,27 @@
+let toggleFullScreen = async () => {
+	if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {  
+      document.documentElement.requestFullScreen();  
+    } else if (document.documentElement.mozRequestFullScreen) {  
+      document.documentElement.mozRequestFullScreen();  
+    } else if (document.documentElement.webkitRequestFullScreen) {  
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+    }  
+  } else {  
+    if (document.cancelFullScreen) {  
+      document.cancelFullScreen();  
+    } else if (document.mozCancelFullScreen) {  
+      document.mozCancelFullScreen();  
+    } else if (document.webkitCancelFullScreen) {  
+      document.webkitCancelFullScreen();  
+    }  
+  }  
+
+}
+
+document.getElementById('gallery').addEventListener('click', toggleFullScreen)
+
 // let start = Date.now(); // remember start time
 
 // let timer = setInterval(function() {
